4832a82f1653289e1458846e0c9cfb365676d800
Nishant <nishantyt0@gmail.com>
2022-11-16 19:03:12 +0545

    chore : added countries model

---
+++ lib/data/raw_data/countries_info.dart
@@ -0,0 +1,31 @@
+import 'package:qatar_pinko_cup/core/app_export.dart';
+import 'package:qatar_pinko_cup/data/models/country_model/country_model.dart';
+
+class CountriesInfo {
+  static Map<String, CountryModel> countriesInfo = {
+    "0": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+    "1": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+    "2": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+    "3": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+    "4": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+    "5": CountryModel(
+      name: 'Qatar',
+      logo: ImageConstant.qatarImage,
+    ),
+  };
+}
