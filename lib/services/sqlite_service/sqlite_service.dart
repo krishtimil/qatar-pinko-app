ed2d36b9345a00088b185c203da1e07a28df7a3f
Nishant <nishantyt0@gmail.com>
2022-11-18 13:04:30 +0545

    feat : added sqlite

--- lib/services/sqlite_service/sqlite_service.dart
+++ lib/services/sqlite_service/sqlite_service.dart
@@ -1,18 +1,38 @@
 import 'package:sqflite/sqflite.dart';
 
 class SqliteService {
-  late Database db;
+  Database? db;
   Future<void> openDb() async {
     db = await openDatabase('${getDatabasesPath()}/main.db', version: 1,
         onCreate: (Database db, int version) async {
-      // When creating the db, create the table
       await db.execute(
-          'CREATE TABLE Pinko (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+          'CREATE TABLE Pinko (id INTEGER PRIMARY KEY, team TEXT, bet INTEGER)');
+      await db.execute('CREATE TABLE RemainingBlc (remainingBlc INTEGER)');
     });
   }
 
-  insertDataIntoDB(int name) async {
+  insertDataIntoDB() async {
     await openDb();
-    await db.execute('INSERT ');
+    int id1 = await db!.rawInsert(
+        'INSERT INTO Pinko (team, bet, remainingBlc) VALUES("Qatar", 1234, 1000)');
   }
+
+  getDataFromDB() async {
+    await openDb();
+    List<Map> list = await db!.rawQuery('SELECT * FROM PINKO');
+    return list;
+  }
+
+  getTotalBet() async {
+    List data = await getDataFromDB();
+    int bet = 0;
+    for (var i = 0; i < data.length; i++) {
+      bet = bet + (data[i]['bet'] as int);
+    }
+    print(bet);
+    print(await getDataFromDB());
+    return bet;
+  }
+
+  
 }
